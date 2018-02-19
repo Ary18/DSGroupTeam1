71cec22b95171bfd02e673a60dee8f1d39b10bed
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-02-19 16:39:58 +0100

    creato contenitore mappa

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function loadWeather(){
     var weather=new extractWeather();
     //console.log(weather.base);
+
+    var mapProp = {
+        center: new google.maps.LatLng(9.00000, 10),
+        zoom: 16,
+    };
+    var map = new google.maps.Map(document.getElementById("mappa"), mapProp);
+    var marker = new google.maps.Marker({
+        position: mapProp.center,
+        map: map
+    });
 }
