508c2ff195b300f055fcc6f8e7618fc9d3f94487
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-02-19 17:29:55 +0100

    inserito localStorage

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function loadWeather(){
     var weather=new extractWeather();
     //console.log(weather.base);
-
     var mapProp = {
         center: new google.maps.LatLng(9.00000, 10),
         zoom: 16,
@@ -14,6 +13,21 @@ function loadWeather(){
         map: map
     });
     loadPosition();
+    var date = new Date();
+    if(localStorage && localStorage['ultimoaccesso']){
+        var oldDate= localStorage['ultimoaccesso'];
+    }else{
+        var oldDate="MAI";
+    }
+    localStorage.setItem('ultimoaccesso', date.toString());
+    if (localStorage && localStorage['username']) {
+        user = localStorage['username'];
+        alert("ho trovato la posizione nello storage");
+    } else {
+        user = 'Mario';
+        localStorage['username'] = user;
+    }
+    alert(user +" ultimo accesso "+ oldDate);
 }
 
 
@@ -37,7 +51,7 @@ function funzioneOk(position) {
         var geocoder = new google.maps.Geocoder;
         geocoder.geocode({'location':mapProp.center}, function (results, status){
             if(results[1]){
-                alert(results[1].formatted_address);
+                alert(results[1].formatted_address);// stringa geocoder reverse
             }else{
                 alert('No Result');
             }
