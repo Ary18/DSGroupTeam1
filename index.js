cd6e1499d9ece60ce6e118262a6fb5dfac060acf
Vanessa Militello <militello.vane@gmail.com>
2018-02-20 11:03:56 +0100

    aggiunto binding pagina statica

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var currentPosition='';
-var oldAccess='';
+var currentPosition = '';
+var oldAccess = '';
+var latitulong = '';
 function loadWeather() {
     var weather = new extractWeather();
     var mapProp = {
@@ -13,7 +14,7 @@ function loadWeather() {
         position: mapProp.center,
         map: map
     });
-    loadPosition(); 
+    loadPosition();
     var date = new Date();
     if (localStorage && localStorage['ultimoaccesso']) {
         var oldDate = localStorage['ultimoaccesso'];
@@ -27,9 +28,19 @@ function loadWeather() {
         user = 'Mario';
         localStorage['username'] = user;
     }
-    oldAccess=user + " ultimo accesso " + oldDate;
+    oldAccess = user + " ultimo accesso " + oldDate;
+
+    var arrayId = ['nome', 'accesso', 'icona', 'temp', 'wind', 'cloudiness', 'pressure', 'humidity', 'sunrise', 'sunset'];
+    var arrayValue = [user, oldDate, weather.weather.icon, weather.main.temp, weather.wind.speed, weather.clouds.all, weather.main.pressure, weather.main.humidity, weather.sys.sunrise, weather.sys.sunset];
+    for (var i = 0; i < arrayId.length; i++) {
+        load(arrayId[i], arrayValue[i]);
+
+    }
+
     //alert(oldAccess);
 }
+
+
 function loadPosition() { //geolocalizza e restituisce l'indirizzo utilizzando il reverse geocoding
     var coordinate;
     if ("geolocation" in navigator) {
@@ -47,11 +58,17 @@ function funzioneOk(position) {
             center: new google.maps.LatLng(latitudine, longitudine),
             zoom: 16,
         };
+        latitulong = mapProp.center;
+        var name = document.getElementById('geoCoords');
+        name.innerText=latitulong;
+        
         var geocoder = new google.maps.Geocoder;
         geocoder.geocode({ 'location': mapProp.center }, function (results, status) {
             if (results[1]) {
-                currentPosition=results[1].formatted_address;
-                alert(currentPosition);// stringa geocoder reverse
+                currentPosition = results[1].formatted_address;
+                var luogo = document.getElementById('luogo');
+                luogo.innerText=currentPosition;
+            
             } else {
                 alert('No Result');
             }
@@ -68,9 +85,12 @@ function funzioneErrore(error) {
     alert(error.message);
 }
 
-function load(id,value){
-    //var name = document.getElementById(id);
+function load(id, value) {
+    var name = document.getElementById(id);
     if (id === 'image') {
         name.src = value;
-    } else { name.innerText = value }
+    } else {
+    name.innerText = value;
+    console.log('foazdsfsaasr');
+    }
 }
