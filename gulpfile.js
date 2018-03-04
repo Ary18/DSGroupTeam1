860e6b2c2232d7253046282b6b8c9e6a2c5f85dd
Vittorio Falconeta <falconeta@iMac-di-Vittorio.local>
2018-03-04 17:59:52 +0100

    aggiornato task build gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var concat=require('gulp-concat');
 var uglify=require('gulp-uglify');
 var jshint=require ('gulp-jshint');
 var replace =require('gulp-string-replace');
+let cleanCSS = require('gulp-clean-css');
 
 const sass =require('gulp-sass');
 
@@ -16,29 +17,33 @@ gulp.task('default', function(){
 
 
 gulp.task('jshint', function(){
-    return gulp.src('*.js').pipe(jshint())
+    return gulp.src('source/javascript/*.js').pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 
 gulp.task('build', function () {
     gulp.src('source/html/*.html')
-        .pipe(replace('../css/style.css', 'style.css'))
-        .pipe(replace('../../node_modules/jquery-easy-loading/dist/jquery.loading.css', '../node_modules/jquery-easy-loading/dist/jquery.loading.css'))
+        .pipe(replace('../css/styleBootstrap.css', 'styleBootstrap.css'))
+        .pipe(replace('../img/logo.png', 'img/logo.png'))
         .pipe(replace('<script src="../../node_modules/jquery/dist/jquery.js"></script>', '<script src="../node_modules/jquery/dist/jquery.js"></script>'))
         .pipe(replace('<script src="../javascript/index.js"></script>', '<script src="bundle.js"></script>'))
-        .pipe(replace('<script src="../../node_modules/jquery-easy-loading/dist/jquery.loading.js"></script>', '<script src="../node_modules/jquery-easy-loading/dist/jquery.loading.js"></script>'))
-        .pipe(replace('../../node_modules/jquery-easy-loading/dist/jquery.loading.css', '../node_modules/jquery-easy-loading/dist/jquery.loading.css'))
+        .pipe(replace(' <script src="../../node_modules/tether/dist/js/tether.js"></script>', ' <script src="../node_modules/tether/dist/js/tether.js"></script>'))
+        .pipe(replace('../../node_modules/bootstrap/dist/css/bootstrap.css', '../node_modules/bootstrap/dist/css/bootstrap.css'))
         .pipe(replace('<script src="../../node_modules/moment/locale/it.js"></script>', '<script src="../node_modules/moment/locale/it.js"></script>'))
         .pipe(replace('<script src="../../node_modules/moment/min/moment.min.js"></script>', ' <script src="../node_modules/moment/min/moment.min.js"></script>'))
-        .pipe(replace('<script src="../../node_modules/jquery-easy-loading/dist/jquery.loading.js"></script>', ' <script src="../node_modules/jquery-easy-loading/dist/jquery.loading.js"></script>'))
+        .pipe(replace('<script src="../../node_modules/bootstrap/dist/js/bootstrap.js"></script>', ' <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>'))
         .pipe(gulp.dest('dist'));
-    gulp.src('source/css/*.css').pipe(gulp.dest('dist'));
+    gulp.src('source/css/*.css')
+    .pipe(replace('../img/load.gif', 'img/load.gif'))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist'));
     gulp.src('source/javascript/*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
         .pipe(uglify()).pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
+    gulp.src('source/img/*').pipe(gulp.dest('dist/img'));
 });
 
     gulp.task('sass', function(){
